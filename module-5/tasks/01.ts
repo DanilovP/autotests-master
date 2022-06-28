b43ab86bcb06327cb1f784ac92f550958fdf23b1
DanilovP <danilovpv98@gmail.com>
2022-06-28 21:56:38 +0300

    master

--- module-5/tasks/01.ts
+++ module-5/tasks/01.ts
@@ -1,7 +1,15 @@
-// Выполнить
+// Выполнить    
 // 0 - перейти на страницу https://testspace.orange.local.learn.ispringdev.com/login
 // 1 - выбрать элемент формы по классу form
 // 2 - выбрать элемент с id равным loginField внутри формы
 // 3 - выбрать элемент с id равным passwordField внутри формы
 // Вывести элементы в консоль
-
+describe(`Test`,function () {
+    it(``,function(){
+        browser.url(`https://testspace.orange.local.learn.ispringdev.com/login`)
+        
+        const form = $(`div[@class="form"]`)
+        console.log(form)
+    })
+    
+})
